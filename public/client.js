0570289fedd7a29b0ec7ebbd1fe491718a262432
Glitch (hello-express) <none>
2019-03-19 16:43:24 +0000

    🎑🚋 Checkpoint ./public/client.js:3109048/219

--- public/client.js
+++ public/client.js
@@ -11,7 +11,8 @@ console.log('hello world :o');
     let tl1 = new TimelineMax();
     
     
-    tl1.from('#myAd_txt2', 1.3, { top: -100,  ease: Power2.easeOut, y: -500  })
-      .to();
+    tl1.from('#myAd_txt2', 1.3, { top: -100,  ease: Power2.easeOut  })
+      .to('#myAd_txt1', 1.3, { top: 210, ease: Power2.easeOut  }, '-=.9')
+      .from('#myAd_marker', 1.2, {  });
     
   }());
