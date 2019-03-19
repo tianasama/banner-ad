dda8ca36cfcbfd15a7488159656359780690dff6
Glitch (hello-express) <none>
2019-03-19 16:22:24 +0000

    💰👦 Checkpoint ./public/client.js:3109048/1344 ./public/style.css:3109048/116

--- public/client.js
+++ public/client.js
@@ -1,42 +1,17 @@
 // client-side js
 // run by the browser each time your view template is loaded
+/* global TimelineMax, Power2 */
+
 
 console.log('hello world :o');
 
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
+// javaaascripttttt
+  (function(){
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
-});
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
+    let tl1 = new TimelineMax();
+    
+    
+    tl1.from('#myAd_txt2', 1.3, { top: -100,  ease: Power2.easeOut, y: -500  })
+      .to();
+    
+  }());
