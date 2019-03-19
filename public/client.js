3b4fb9e3121a0d8ba1115e4faf89cb1e48058ba7
Glitch (hello-express) <none>
2019-03-19 16:53:25 +0000

    🚩🎿 Checkpoint ./public/client.js:3109048/524

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 // client-side js
 // run by the browser each time your view template is loaded
-/* global TimelineMax, Power2 */
+/* global TimelineMax, Power2, Bounce, Back */
 
 
 console.log('hello world :o');
@@ -13,6 +13,11 @@ console.log('hello world :o');
     
     tl1.from('#myAd_txt2', 1.3, { top: -100,  ease: Power2.easeOut  })
       .to('#myAd_txt1', 1.3, { top: 210, ease: Power2.easeOut  }, '-=.9')
-      .from('#myAd_marker', 1.2, {  });
+      .from('#myAd_marker', 1.2, { top:-110, ease: Bounce.easeOut}, '-=.5')
+      .from('#myAd_shadow', 1.2, { opacity: 0, ease: Bounce.easeOut }, '-=1.2')
+      .from('#myAd_txt3', 1.2, { scale:.2, opacity: 0, ease: Power2.easeOut }, '-=.9')
+      .from('#myAd_surfboard', 1.3, { top: 260, rotation: -130, left: 300, ease: Back.easeOut }, 'cta');
     
+      // tl1.seek('cta');
+      
   }());
